16257058c4268c4e2dade41d039f72cc75e2ee8f
leche <96605480+leche64@users.noreply.github.com>
2025-02-28 11:31:09 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,7 +8,7 @@ import { House, TwitterLogo, GithubLogo, Book } from "@phosphor-icons/react";
 export function NavBar() {
   const pathname = usePathname();
   
-  return (
+  return ( 
     <nav className="fixed top-0 z-50 w-full bg-transparent">
       <div className="w-full flex h-16 md:h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
         {/* Logo and Brand Name */}
