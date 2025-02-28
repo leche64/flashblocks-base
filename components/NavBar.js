f663c6229ebd90ed765c191b0a481a9d3c9364eb
leche <96605480+leche64@users.noreply.github.com>
2025-02-28 11:13:22 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,7 +10,7 @@ export function NavBar() {
   
   return (
     <nav className="fixed top-0 z-50 w-full bg-transparent">
-      <div className="container flex h-16 md:h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="w-full flex h-16 md:h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
         {/* Logo and Brand Name */}
         <div className="flex items-center gap-2 md:gap-3">
           <Link href="/" className="flex items-center gap-2 md:gap-3">
